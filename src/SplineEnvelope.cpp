8e232f746ef1d86a4a20b6da5f7cae33755091cf
David Gunnarsson <guda@gudaaudio.com>
2017-01-01 20:05:27 +0100

    added splineenvelope

---
+++ src/SplineEnvelope.cpp
@@ -0,0 +1,124 @@
+/*
+  ==============================================================================
+
+    SplineEnvelope.cpp
+    Created: 1 Sep 2016 6:06:16pm
+    Author:  David Gunnarsson
+
+  ==============================================================================
+*/
+
+#include "SplineEnvelope.h"
+#include "globalStuff.h"
+
+void SplineEnvelope::updateFromCachedData(const int sampleFrames) {
+    const ScopedLock sl (lock);
+    if(ready()) {
+        isReady = false;
+        updateNrOfSamples(sampleFrames);
+        isReady = true;
+    } else {
+        DBUG(("have no cached data to use!"));
+    }
+}
+
+void SplineEnvelope::updateNrOfSamples(const int sampleFrames) {
+    const ScopedLock sl (lock);
+    buffer.clear(0, sampleFrames);
+    sharedSplineData.needToSyncValue = true;
+    splineOscillator.reset();
+    splineOscillator.calculateFullEnvelope(sampleFrames, buffer.getWritePointer(0));
+    bufferedSampleFrames = sampleFrames;
+    everUpdated = true;
+    lastSampleRate = g_samplerate;
+}
+
+bool SplineEnvelope::checkNeedToUpdate(double* splineAmplitudeData, const int sampleFrames) {
+    const ScopedLock sl (lock);
+    if(!everCheckedNeedToUpdate) {
+        everCheckedNeedToUpdate = true;
+        return true;
+    }
+    
+    if(lastSampleRate != g_samplerate) {
+        return true;
+    }
+
+    Osc4Data tmpOsc4Data[MAX_OSC4_POINTS];
+    updateOsc4DataFromParams(tmpOsc4Data, splineAmplitudeData, sharedSplineData);
+    for(int i = 0 ; i < MAX_OSC4_POINTS ; i++) {
+        if(osc4Data[i] != tmpOsc4Data[i]) {
+            DBUG(("osc4Data point %i changed", i));
+            return true;
+        }
+    }
+    if(bufferedSampleFrames == sampleFrames) {
+        return false;
+    }
+    
+    if(sampleFrames == sampleFramesForThread && (needToUpdateFromThreadBuffer || needReRunThread || isThreadRunning())) {
+        DBUG(("needToUpdateFromThreadBuffer %i, needReRunThread %i, isThreadRunning() %i", needToUpdateFromThreadBuffer, needReRunThread, isThreadRunning()));
+        //will be updated next time updateFromBufferCacheIfNeeded is run
+        return false;
+    }
+    
+    DBUG(("bufferedSampleFrames %i, sampleFrames %i, sampleFramesForThread %i, needToUpdateFromThreadBuffer %i", bufferedSampleFrames, sampleFrames, sampleFramesForThread, needToUpdateFromThreadBuffer));
+    return true;
+}
+
+static CriticalSection threadLock; //just to only process one SplineEnvelope thread at once, more to avoid cpu burst than for safety
+void SplineEnvelope::run() {
+    DBGRAII;
+    do {
+        DBUG(("start round"));
+        needReRunThread = false;
+        if(sampleFramesForThread == 0) {
+            DBUG(("WARNING: sampleFramesForThread == 0"));
+            return;
+        }
+        threadBufferOKToCopy = false;
+        lastSampleRate = g_samplerate;
+        {
+            const ScopedLock sl(lock);
+            const ScopedLock tl (threadLock);
+            for(int i = 0 ; i < MAX_OSC4_POINTS ; i++) {
+                threadOsc4Data[i] = osc4Data[i];
+            }
+        }
+        {
+            const ScopedLock tl (threadLock);
+            
+            threadSplineOscillator.reset();
+            threadSplineOscillator.calculateFullEnvelope(sampleFramesForThread, threadBuffer.getWritePointer(0));
+            everUpdated = true;
+        }
+        
+        threadBufferOKToCopy = true;
+        needToUpdateFromThreadBuffer = true;
+        if(needReRunThread) {
+            DBUG(("needReRunThread is true, wait some before next round"));
+            wait(100);
+        }
+    } while (needReRunThread);
+}
+
+bool SplineEnvelope::updateFromBufferCacheIfNeeded() {
+    if(!threadBufferOKToCopy && needToUpdateFromThreadBuffer) {
+        return false;
+    }
+    const ScopedLock sl (lock);
+    DBGRAII;
+    isReady = false;
+    buffer.copyFrom(0, 0, threadBuffer.getReadPointer(0), sampleFramesForThread);
+    bufferedSampleFrames = sampleFramesForThread;
+    needToUpdateFromThreadBuffer = false;
+    isReady = true;
+    return true;
+}
+
+
+
+
+
+
+
---
+++ src/SplineEnvelope.h
@@ -0,0 +1,95 @@
+/*
+  ==============================================================================
+
+    SplineEnvelope.h
+    Created: 1 Sep 2016 6:06:16pm
+    Author:  David Gunnarsson
+
+  ==============================================================================
+*/
+
+#ifndef SPLINEENVELOPE_H_INCLUDED
+#define SPLINEENVELOPE_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "SplineOscillator.h"
+#include "SplineOscillatorMisc.h"
+#include "SamplePlayerVoice.h"
+
+//Precalculated envelope class based on SplineOscillator
+class SplineEnvelope : public Thread
+{
+public:
+    SplineEnvelope()
+    : Thread("SplineEnvelope"),
+      buffer(1, MAX_SAMPLE_VOICE_LENGTH_IN_SAMPLES),
+      threadBuffer(1, MAX_SAMPLE_VOICE_LENGTH_IN_SAMPLES)
+    {
+        const ScopedLock sl (lock);
+        splineOscillator.setOsc4Data(&osc4Data);
+        splineOscillator.setSharedSplineData(&sharedSplineData);
+        
+        threadSplineOscillator.setOsc4Data(&threadOsc4Data);
+        threadSplineOscillator.setSharedSplineData(&sharedSplineData);
+    };
+    
+    ~SplineEnvelope() {
+        waitForThreadToExit(1000);
+    }
+    
+    //true if anything updated
+    bool update(double* splineAmplitudeData, const int sampleFrames, const bool asyncOK = false) {
+        const ScopedLock sl (lock);
+        bool needToUpdate = checkNeedToUpdate(splineAmplitudeData, sampleFrames);
+        if(needToUpdate) {
+            updateOsc4DataFromParams(osc4Data, splineAmplitudeData, sharedSplineData);
+            
+            if(asyncOK && isThreadRunning()) {
+                needReRunThread = true;
+            } else if(asyncOK && everUpdated) {
+                sampleFramesForThread = sampleFrames;
+                startThread(0);
+            } else {
+                updateNrOfSamples(sampleFrames);
+            }
+        }
+        isReady = true;
+        return needToUpdate;
+    }
+    
+    AudioSampleBuffer* getBuffer() {return &buffer;};//TODO: ska vara smart att ge tillbaka en buffer som inte håller på att uppdateras.
+    bool ready() {return isReady;};
+    void updateFromCachedData(const int sampleFrames);
+    CriticalSection lock;
+    
+    virtual void run() override;
+    bool updateFromBufferCacheIfNeeded();//return true if updated
+
+private:
+    bool checkNeedToUpdate(double* splineAmplitudeData, const int sampleFrames);
+    void updateNrOfSamples(const int sampleFrames);
+    int bufferedSampleFrames = 0;
+
+    SplineOscillator splineOscillator;
+    SharedSplineData sharedSplineData;
+    Osc4Data osc4Data[MAX_OSC4_POINTS];
+    AudioSampleBuffer buffer;
+    
+    SplineOscillator threadSplineOscillator;
+    AudioSampleBuffer threadBuffer;
+    int sampleFramesForThread = 0;
+    Osc4Data threadOsc4Data[MAX_OSC4_POINTS];
+    bool threadBufferOKToCopy = false;
+    bool needToUpdateFromThreadBuffer = false;
+    bool needReRunThread = false;
+    
+    bool isReady = false;
+    bool everCheckedNeedToUpdate = false;
+    bool everUpdated = false;
+    float lastSampleRate = 0;
+    JUCE_LEAK_DETECTOR (SplineEnvelope)
+};
+
+
+
+#endif  // SPLINEENVELOPE_H_INCLUDED
